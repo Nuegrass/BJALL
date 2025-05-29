7936a17e8f95709a050ed23117fc44c9bff7e1f5
TimedT <PasangSherpa10402@gmail.com>
2025-05-29 01:13:02 -0700

    GUI

--- Assets/Scripts/ChangeScene.cs
+++ Assets/Scripts/ChangeScene.cs
@@ -1,3 +1,4 @@
+using System;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
@@ -6,5 +7,15 @@ public class ChangeScene : MonoBehaviour
     public void MoveToScene(string scenePath)
     {
         SceneManager.LoadScene(scenePath);
+    }
+
+    public void AddScene(string scenePath)
+    {
+        SceneManager.LoadScene(scenePath, LoadSceneMode.Additive);
+    }
+
+    public void UnloadScene(string scenePath)
+    {
+        SceneManager.UnloadSceneAsync(scenePath);
     }
 }
